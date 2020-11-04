3a168b4e60ccb74c897c9b874887bd9866b556c1
Lawlighty <49227867+Lawlighty@users.noreply.github.com>
2020-11-04 10:39:32 +0800

    Add files via upload

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,5 +1,5 @@
 import styles from "./layout.module.css";
-import Head from "next/Head";
+import Head from "next/head";
 import utilStyles from "../../styles/utils.module.css";
 import Link from "next/link";
 
